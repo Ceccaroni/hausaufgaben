2032385f07448cdb87d86ee180704f6500e3fe7e
Ceccaroni <adrian.lanz@me.com>
2025-06-04 06:47:50 +0200

    Clean up repo and improve auth

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,17 @@
-const CACHE = 'hausaufgaben-v1';
+const CACHE = 'hausaufgaben-v2';
 const assets = [
+  'index.html',
+  'admin.html',
   'sus.html',
   'manifest.json',
   'service-worker.js',
-  'assets/logo-huttwil.svg'
+  'assets/logo-huttwil.svg',
+  'public/fade.css',
+  'public/style-index.css',
+  'public/style-shared.css',
+  'public/style-sus.css',
+  'public/admin.js',
+  'public/sus.js'
 ];
 self.addEventListener('install', e =>
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(assets)))
