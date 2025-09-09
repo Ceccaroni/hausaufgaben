330d764f5f1653505c0d24438ed82e5c33396c10
Ceccaroni <adrian.lanz@me.com>
2025-09-09 16:51:46 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 /* Datei: service-worker.js – Netzwerk-first für HTML/JS, SW-safe Caching */
 
-const CACHE_NAME = 'hausaufgaben-v11'; // <— bei Änderungen hochzählen!
+const CACHE_NAME = 'hausaufgaben-v12'; // <— bei Änderungen hochzählen!
 
 self.addEventListener('install', (event) => {
   // sofort aktiv werden
